016cd4101c03a71fc346d0bb9169dd874b46d544
bozberkeoz <bozberkeoz>
2024-09-14 03:51:27 -0700

    EmployeeDataChange

--- dbo/Functions/Creation-Scripts/GetEmployeeDetails.sql
+++ dbo/Functions/Creation-Scripts/GetEmployeeDetails.sql
@@ -2,15 +2,23 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE FUNCTION dbo.GetEmployeeDetails(@EmployeeID INT)
+CREATE FUNCTION dbo.GetEmployeeDetails(@EmployeeID INT = NULL)
 RETURNS NVARCHAR(150)
 AS
 BEGIN
     DECLARE @Details NVARCHAR(150);
-    SELECT @Details = COALESCE(FirstName + ' ' + LastName + ', Department: ' + Department, 'Employee Not Found')
-    FROM Employees 
-    WHERE EmployeeID = @EmployeeID;
+
+    IF @EmployeeID IS NULL
+    BEGIN
+        SET @Details = 'No Employee ID provided.';
+    END
+    ELSE
+    BEGIN
+        SELECT @Details = COALESCE(FirstName + ' ' + LastName + ', Department: ' + Department, 'Employee Not Found')
+        FROM Employees 
+        WHERE EmployeeID = @EmployeeID;
+    END
+
     RETURN @Details;
 END;
 GO
