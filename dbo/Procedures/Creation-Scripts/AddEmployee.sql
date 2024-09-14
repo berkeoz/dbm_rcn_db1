a1d78014701b8934d13e0225250ff5c3a7585e8b
berkeoz <berkeoz>
2024-09-14 03:18:26 -0700

    FunctionAddition

--- dbo/Procedures/Creation-Scripts/AddEmployee.sql
+++ dbo/Procedures/Creation-Scripts/AddEmployee.sql
@@ -4,6 +4,12 @@ CREATE PROCEDURE AddEmployee
 @Department NVARCHAR(50)
 AS
 BEGIN
+    IF EXISTS (SELECT 1 FROM Employees WHERE FirstName = @FirstName AND LastName = @LastName)
+    BEGIN
+        PRINT 'Employee already exists';
+        RETURN;
+    END
+
     INSERT INTO Employees (FirstName, LastName, Department)
     VALUES (@FirstName, @LastName, @Department);
 END;
