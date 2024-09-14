8b495e831ab6e2d3c49f1258dca39a0d8efeab9c
berkeoz <berkeoz>
2024-09-14 03:18:44 -0700

    EmployeeChangeLog

--- dbo/Functions/Creation-Scripts/GetEmployeeDetails.sql
+++ dbo/Functions/Creation-Scripts/GetEmployeeDetails.sql
@@ -2,12 +2,13 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE FUNCTION dbo.GetEmployeeDetails(@EmployeeID INT)
 RETURNS NVARCHAR(150)
 AS
 BEGIN
     DECLARE @Details NVARCHAR(150);
-    SELECT @Details = FirstName + ' ' + LastName + ', Department: ' + Department 
+    SELECT @Details = COALESCE(FirstName + ' ' + LastName + ', Department: ' + Department, 'Employee Not Found')
     FROM Employees 
     WHERE EmployeeID = @EmployeeID;
     RETURN @Details;
