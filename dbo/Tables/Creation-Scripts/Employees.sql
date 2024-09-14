d96a52c793e8b727a4967a8aefe8a25159e4b6f0
bozberkeoz <bozberkeoz>
2024-09-14 03:39:56 -0700

    Revert "ColumnAdditionInteverntion; TaskID: ColumnTask"
    This reverts commit 393ca47cff1648361fd7dc468bd2ea6c633a6611.

--- dbo/Tables/Creation-Scripts/Employees.sql
+++ dbo/Tables/Creation-Scripts/Employees.sql
@@ -7,8 +7,7 @@ CREATE TABLE [dbo].[Employees](
 	[FirstName] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[LastName] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[Department] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[HireDate] [datetime2](7) NULL,
-	[Email] [nvarchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[HireDate] [date] NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[EmployeeID] ASC
